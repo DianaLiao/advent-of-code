3f29b6d63d01bfe6d9f4a7fc2fc42b049c7c76e5
Diana Liao <dijali@gmail.com>
2021-12-04 22:57:35 -0500

    add day 4 part 2

--- 2021/2021-04.rb
+++ 2021/2021-04.rb
@@ -1,13 +1,16 @@
 require './input.rb'
-require 'pry'
 
 class BingoBoard
 
   attr_accessor :rows, :win
+  @@last_score = 1
+  @@all = []
 
   def initialize(five_line_array)
     @rows = five_line_array
     @win = false
+
+    @@all << self
   end
 
   def check_number(called_num)
@@ -26,10 +29,6 @@ class BingoBoard
       end
     end
 
-    if @rows[0].length == 0
-      print @rows
-    end
-
     columns = @rows.transpose
     columns.each do |column|
       if column.sum == -5
@@ -46,6 +45,22 @@ class BingoBoard
       row.each{|num| sum += num if num != -1}
     end
     sum
+  end
+
+  def self.set_last_score(new_score)
+    @@last_score = new_score
+  end
+
+  def self.get_last_score
+    @@last_score
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.non_winners
+    BingoBoard.all.reject{|board| board.win}
   end
 
 end
@@ -73,7 +88,7 @@ def parse_input(input)
   {boards: bingo_boards, numbers_to_call: numbers_to_call}
 end
 
-# one day I'll learn to write real unit tests
+# one day I'll learn to write real tests
 # test = BingoBoard.new([[22, 13, 17, 11, 0], [8, 2, 23, 4, 24], [21, 9, 14, 16, 7], [6, 10, 3, 18, 5], [1, 12, 20, 15, 19]])
 
 # [13,9,2,10,12,15].each do |num|
@@ -112,8 +127,34 @@ def play_bingo(input)
   puts score
 end
 
-# play_bingo(Input.day_4_sample_input)
-play_bingo(Input.day_4_input)
+def let_the_squid_win(input)
+  game_items = parse_input(input)
+  numbers_to_call = game_items[:numbers_to_call]
+  boards = game_items[:boards]
 
-# game_info = parse_input(Input.day_4_input)
-# print game_info[:boards].count
+  current_num_index = 0
+  score = 0
+
+  numbers_to_call.length.times do
+    num = numbers_to_call[current_num_index]
+  
+    BingoBoard.non_winners.each do |board|
+      board.check_number(num)
+      winner = board.check_win
+      if winner 
+        score = num * board.board_score
+        BingoBoard.set_last_score(score)
+      end
+    end
+
+    current_num_index += 1
+  end
+
+  puts BingoBoard.get_last_score
+end
+
+# play_bingo(Input.day_4_sample_input)
+# play_bingo(Input.day_4_input)
+# let_the_squid_win(Input.day_4_sample_input)
+let_the_squid_win(Input.day_4_input)
+
